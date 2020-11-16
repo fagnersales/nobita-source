351592ebb21b6fc9904e43a43095277bcf7c0a23
Fagner Sales <fs.fagnersales@gmail.com>
2020-11-16 11:15:13 -0300

    Update: Deleted v1.0 files & Installed dependences

--- index.js
+++ index.js
@@ -1,36 +1,8 @@
 require('dotenv').config()
-require('./database')
-
-const GitHub = require('./github')
-
-GitHub.setCredentials({
-    username: process.env.GITHUB_USERNAME,
-    password: process.env.GITHUB_PASSWORD,
-    token: process.env.GITHUB_TOKEN
-})
-
-const prefix = process.env.PREFIX
 
 const { Client } = require('discord.js')
 
-const { join } = require('path')
-
 const client = new Client()
-
-const insertCommands = require('./Utils/insertCommands')
-
-insertCommands(client, join(__dirname, 'commands'))
-
-client.on('ready', _ => {
-    console.log(`Ready to work.`)
-    require('./Tests/command')(client, prefix)
-})
-
-client.on('message', message => {
-    if (message.content === 'Fagner') message.reply(`My god.`)
-
-    if (message.content.startsWith(prefix)) require('./events/command')(client, message, prefix)
-})
 
 
 client.login(process.env.TOKEN)
--- nibo/createCircleMask.js
+++
@@ -1,8 +0,0 @@
-const createRec = require("./createRectangle")
-
-module.exports = (w, h) => {
-    const full = createRec(w, h, 'black')
-    const minor = createRec(w, h, 'white')
-    minor.circle({ radius: (w + h) / 4 + 1, x: minor.getWidth() / 2, y: minor.getHeight() / 2 })
-    return full.composite(minor, 0, 0)
-}
